0dedd722ddc3d676c8acc16dfb5e5287c3835810
Arthur Machado <arthur.machado@maxmilhas.com.br>
2024-02-10 12:50:58 -0300

    feat: add redis and improve rotes

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "crypto";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -33,7 +34,10 @@ export async function voteOnPoll(app: FastifyInstance) {
           where: {
             id: userPreviousVoteOnPoll.id
           }
-        })
+        });
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
+
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({message: "You already voted on this poll."})
       }
@@ -57,6 +61,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId
       }
     });
+
+    await redis.zincrby(pollId, 1, pollOptionId);
   
     return reply.status(201).send();
   
